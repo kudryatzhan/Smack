9bbd981c5811d2d8d96a97a7950a1af5084ae49e
Kudryatzhan Arziyev <karziyev@gmail.com>
2017-12-14 15:19:56 -0700

    Create account button set up, register function configured

--- Smack/Controller/CreateAccountViewController.swift
+++ Smack/Controller/CreateAccountViewController.swift
@@ -8,15 +8,53 @@
 
 import UIKit
 
-class CreateAccountViewController: UIViewController {
-
+class CreateAccountViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var userImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         
+        usernameTextField.delegate = self
+        emailTextField.delegate = self
+        passwordTextField.delegate = self
     }
     
     @IBAction func closePressed(_ sender: UIButton) {
         performSegue(withIdentifier: UNWIND, sender: nil)
     }
+    
+    @IBAction func createAccountButtonPressed(_ sender: UIButton) {
+        guard
+            let email = emailTextField.text,
+            !email.isEmpty,
+            let password = passwordTextField.text,
+            !password.isEmpty else { return }
+        
+        AuthService.instance.registerUser(email: email, password: password) { (success) in
+            
+            if success {
+                print("Registered user successfully")
+            } else {
+                print("Could not register user")
+            }
+        }
+        
+    }
+    
+    @IBAction func chooseAvatarButtonPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func generateBgColorButtonPressed(_ sender: UIButton) {
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    
 }
