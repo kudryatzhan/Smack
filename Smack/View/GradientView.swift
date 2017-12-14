6fd9c8891b45f88ab94163345efee84657b140a8
Kudryatzhan Arziyev <karziyev@gmail.com>
2017-12-13 22:51:13 -0700

    Custom Gradient View set

---
+++ Smack/View/GradientView.swift
@@ -0,0 +1,36 @@
+//
+//  GradientView.swift
+//  Smack
+//
+//  Created by Kudryatzhan Arziyev on 12/13/17.
+//  Copyright © 2017 Kudryatzhan Arziyev. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class GradientView: UIView {
+    
+    @IBInspectable var topColor: UIColor = #colorLiteral(red: 0.2901960784, green: 0.3019607843, blue: 0.8470588235, alpha: 1) {
+        didSet {
+            setNeedsLayout()
+        }
+    }
+    
+    @IBInspectable var bottomColor: UIColor = #colorLiteral(red: 0.1725490196, green: 0.831372549, blue: 0.8470588235, alpha: 1) {
+        didSet {
+            setNeedsLayout()
+        }
+    }
+    
+    override func layoutSubviews() {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [topColor.cgColor, bottomColor.cgColor]
+        gradientLayer.startPoint = CGPoint.zero
+        gradientLayer.endPoint = CGPoint(x: 1, y: 1)
+        gradientLayer.frame = self.bounds
+        
+        self.layer.insertSublayer(gradientLayer, at: 0)
+    }
+    
+}
